7422995d4512a78a658d37900b6fe302ad6bc588
Leo Picado <leopic@gmail.com>
2014-01-19 13:34:26 -0600

    Adding unit tests, hope this works (not really).

---
+++ src/test/java/hello/GreetingTest.java
@@ -0,0 +1,21 @@
+package hello;
+
+import static org.junit.Assert.fail;
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+@SuppressWarnings("deprecation")
+public class GreetingTest {
+
+	@SuppressWarnings("deprecation")
+	@Test
+	public void testSumar() {
+		Greeting greeting = new Greeting(0, null);
+		int resultado = greeting.sumar(1, 1);
+		Assert.assertEquals(2, resultado); // works
+		Assert.assertEquals(1, resultado); // breaks
+		fail("Not yet implemented");
+	}
+
+}
